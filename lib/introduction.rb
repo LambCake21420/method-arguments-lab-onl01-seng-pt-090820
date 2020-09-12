268fe95ec0782090ddc8e16e1a061ed297b4fbf5
Cayla Luiz <amidskycayla@gmail.com>
2020-09-12 20:22:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,8 +4,8 @@ end
 
 introduction("Josh")
 
-def introduction_with_languages(name, language)
-  puts "Hi, my name is #{name}and I am learning to program in #{language}."
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_languages("Dan", "Ember.js")
